e2695024e558aa18641061dd501769729c10ead2
Teddy Minaya <teddyminaya@gmail.com>
2022-12-23 00:07:13 -0500

    commit tree b66f712c413098ea5cfebb35bafc33435fbc6270

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names,event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+      const message=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message)
+    }
+  
+    return messages;
+  }
+  
+  console.log(messages)
+  
+  function countDown () {
+    let countDown= 10;
+  while (countDown >=0)
+  console.log(countDown--);
+  }
